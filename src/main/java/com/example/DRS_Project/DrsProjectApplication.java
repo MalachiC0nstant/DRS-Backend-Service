079fb60d5c2df7e3f12ee4e61768779645bf55ae
Semion Dyadkin <sema.dyadkin@gmail.com>
2024-10-31 15:58:42 -0500

    Basic spring boot template

---
+++ src/main/java/com/example/DRS_Project/DrsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.DRS_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DRS_Project/DrsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DRS_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
