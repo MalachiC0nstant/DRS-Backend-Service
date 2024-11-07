6f473b89e9af27e5f462ac8ef4deb912632b100c
Semion Dyadkin <sema.dyadkin@gmail.com>
2024-11-07 16:50:27 -0600

    Basic spring security base setup.

---
+++ src/main/java/com/example/DRS_Project/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.example.DRS_Project;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception { // TODO: Decide what to do with the throws exception
+        return http
+                .authorizeHttpRequests(authorizeConfig -> {
+                    authorizeConfig.requestMatchers("/").permitAll();
+                    authorizeConfig.requestMatchers("error").permitAll();
+                    authorizeConfig.requestMatchers("favicon.ico").permitAll();
+                    authorizeConfig.anyRequest().authenticated();
+                })
+                .formLogin(withDefaults())
+                .oauth2Login(withDefaults())
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager(
+                User.builder()
+                        .username("user")
+                        .password("{noop}password")
+                        .authorities("ROLE_user")
+                        .build()
+        );
+    }
+}
